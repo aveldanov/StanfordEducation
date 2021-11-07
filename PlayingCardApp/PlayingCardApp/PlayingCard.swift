d06970a6dbe73d295812a2ab90bcfed048e11760
Anton Veldanov <anton.veldanov@gmail.com>
2021-11-07 11:43:48 -0800

    iOS

---
+++ PlayingCardApp/PlayingCardApp/PlayingCard.swift
@@ -0,0 +1,32 @@
+//
+//  PlayingCard.swift
+//  PlayingCardApp
+//
+//  Created by Anton Veldanov on 11/7/21.
+//
+
+import Foundation
+
+struct PlayingCard{
+    
+    let suit: Suit
+    let rank: Rank
+    
+
+
+}
+
+enum Suit{
+    case spades
+    case clubs
+    case diamonds
+    case hearts
+    
+}
+
+
+enum Rank{
+    
+    
+}
+
---
+++ PlayingCardApp/PlayingCardApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayingCardApp
+//
+//  Created by Anton Veldanov on 11/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
