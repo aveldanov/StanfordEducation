aab6d8ea6a3aede5523290f303bf690292e258ad
Anton Veldanov <anton.veldanov@gmail.com>
2021-11-12 23:49:05 -0800

    iOS

---
+++ PlayingCardApp/PlayingCardApp/PlayingCardView.swift
@@ -0,0 +1,16 @@
+//
+//  PlayingCardView.swift
+//  PlayingCardApp
+//
+//  Created by Anton Veldanov on 11/12/21.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+
+}
